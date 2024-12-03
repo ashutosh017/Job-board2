a040fdae8a9a3756ea351c6366c39a85b0d32615
ashutosh017 <wwwashutosh225@gmail.com>
2024-12-03 12:03:46 +0530

    debug

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://job-borad.vercel.app"],
+  origin: ["http://localhost:5173", "https://job-board2-tau.vercel.app/"],
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
